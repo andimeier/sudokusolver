c86873416d55c93f06619ff0fcc769628647d469
Alexander Eck-Zimmer <a.zimmer@gmx.at>
2016-04-30 21:06:16 +0200

    no warnings left but 1 (unused variable)
    unit test is red and it seems it is indeed indicating a problem!

--- test/testTest1.c
+++ test/testTest1.c
@@ -5,8 +5,10 @@
  * Created on 26. April 2016, 20:27
  */
 
+#include <stdlib.h>
 #include "unity.h"
 #include "solve.h"
+#include "util.h"
 
 void test_Dummy(void) {
     TEST_ASSERT_EQUAL(2, 240);
@@ -17,27 +19,38 @@ void test_Dummy2(void) {
     TEST_ASSERT_EQUAL(12, 12);
 }
 
-
 void test_fieldCandidatesSubsetOf(void) {
-    unsigned *candidates = { 0, 2, 3, 4, 5, 6, 0, 0, 0 };
-    Field field = {
-        NULL, // unitPositions
-        candidates,
-        0, // initValue
-        0, // candidatesLeft
-        0, // value;
-        "A2" // name
-    };
-    unsigned *numbers = { 2, 4, 5, 0 };
-    
-    TEST_ASSERT_EQUAL(fieldCandidatesSubsetOf(&field, numbers), 1);
-}
+    unsigned *candidates;
+    Field field;
+    unsigned *numbers;
 
+    candidates = (unsigned *) xmalloc(sizeof (unsigned) * 9);
+    for (unsigned i = 0; i < 9; i++) {
+        candidates[i] = 0;
+    }
+    candidates[1] = 2;
+    candidates[3] = 4;
+    candidates[4] = 5;
+    candidates[5] = 6;
+
+    field.candidates = candidates;
+
+    numbers = (unsigned *) xmalloc(sizeof (unsigned) * 9);
+    numbers[0] = 2;
+    numbers[1] = 4;
+    numbers[2] = 5;
+    numbers[3] = 0;
+
+    TEST_ASSERT_EQUAL(1, fieldCandidatesSubsetOf(&field, numbers));
+
+    free(numbers);
+    free(candidates);
+}
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_Dummy);
-    RUN_TEST(test_Dummy2);
+    //RUN_TEST(test_Dummy);
+    //RUN_TEST(test_Dummy2);
     RUN_TEST(test_fieldCandidatesSubsetOf);
     return UNITY_END();
 }
