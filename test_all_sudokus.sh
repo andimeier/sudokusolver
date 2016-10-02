97a19cd471012fe2d1d77ae8c2673894ac88cce2
Andreas Meier <andimeier@gmx.net>
2016-10-02 17:14:33 +0200

    amended test script for testing all Sudoku examples

--- test_all_sudokus.sh
+++ test_all_sudokus.sh
@@ -21,9 +21,14 @@ function nok {
 	red; echo "FAIL"; normal
 }
 
+echo
+echo "Testing sample Sudokus (should all be solved)"
+echo
+
 # standard examples should all work
 for i in examples/* 
  do 
+   if [ -f $i ]; then
    echo -n "$i: " 
    if ./out/sudoku-solver.exe  $i | grep "HAS BEEN SOLVED" >/dev/null
    then 
@@ -32,12 +37,18 @@ for i in examples/*
   else
     echo -n "+++++ FAIL +++++ => "
 	nok
-  fi 
+  fi
+	fi 
 done
 
+echo
+echo "Testing difficult Sudokus (cannot be solved)"
+echo
+
 # too difficult
-for i in examples/too_difficult 
+for i in examples/too_difficult/*
  do 
+   if [ -f $i ]; then
    echo -n "$i: " 
    if ./out/sudoku-solver.exe  $i | grep "could not be solved" >/dev/null
    then 
@@ -47,4 +58,26 @@ for i in examples/too_difficult
     echo -n "SOLVED? => "
 	nok
   fi 
+  fi
+done
+
+
+echo
+echo "Testing invalid Sudokus (should yield an error)"
+echo
+
+# invalid
+for i in examples/invalid/*
+ do 
+   if [ -f $i ]; then
+   echo -n "$i: " 
+   if ./out/sudoku-solver.exe  $i | grep "ERROR" >/dev/null
+   then 
+     echo -n "ERROR => "
+	 ok
+  else
+    echo -n "NO ERROR => "
+	nok
+  fi 
+  fi
 done
